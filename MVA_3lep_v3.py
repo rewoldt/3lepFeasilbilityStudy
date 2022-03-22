8ced68952139d01830257d6f81bdebf93d267178
Amy Rene Rewoldt <amy.rewoldt@mavs.uta.edu>
2022-03-22 13:54:09 -0500

    MVA update 3/22

--- MVA_3lep_v3.py
+++ MVA_3lep_v3.py
@@ -34,17 +34,37 @@ def PT(n):
 	#takes a particles container and returns the pt
 	return n.p4().Pt()
 
+def selectLeptons(leps):
+  # assume the list is already sorted to start with highest pT
+  first = leps[0]
+  best = None
+  for lep in leps[1:]:
+    if first.pdgId() + lep.pdgId() != 0: continue
+    if best == None: best = lep
+    if first.p4().DeltaR(lep.p4()) < first.p4().DeltaR(best.p4()): best = lep
+  sel = [first, best] if best else [first]
+  for lep in leps:
+    if lep in sel: continue
+    sel.append(lep)
+    break
+  return sel
+
 #################################
 ######### Opening File ##########
 #################################
 
 fileName_ttb = "/cluster/home/bburghgr/truth-ttb/mc16_13TeV.410470.PhPy8EG_A14_ttbar_hdamp258p75_nonallhad.deriv.DAOD_TRUTH1.e6337_p3401/DAOD_TRUTH1.13470208._000496.pool.root.1"
-fileName_benchmark_500 = "/cluster/home/amyrewoldt/DAOD/3lep_Hplus500_slep110_Xn165_Xc120/truth1/DAOD_TRUTH1.aod.pool.root"
-fileName_benchmark_600 = "/cluster/home/amyrewoldt/DAOD/3lep_Hplus600_slep110_Xn165_Xc120/truth1/DAOD_TRUTH1.aod.pool.root"
-fileName_benchmark_700 = "/cluster/home/amyrewoldt/DAOD/3lep_Hplus700_slep110_Xn165_Xc120/truth1/DAOD_TRUTH1.aod.pool.root"
-fileName_benchmark_800 = "/cluster/home/amyrewoldt/DAOD/3lep_Hplus800_slep110_Xn165_Xc120/truth1/DAOD_TRUTH1.aod.pool.root"
-fileName_benchmark_1000 = "/cluster/home/amyrewoldt/DAOD/3lep_benchmark_Hplus1000_slep110/truth1/DAOD_TRUTH1.aod.pool.root"
-fileName_benchmark_800_fixed = "/cluster/home/amyrewoldt/DAOD/3lep_Hplus800_slep110_Xn165_Xc120_fixed/truth1/DAOD_TRUTH1.test.pool.root"
+#fileName_benchmark_500 = "/cluster/home/amyrewoldt/DAOD/3lep_Hplus500_slep110_Xn165_Xc120/truth1/DAOD_TRUTH1.aod.pool.root"
+#fileName_benchmark_600 = "/cluster/home/amyrewoldt/DAOD/3lep_Hplus600_slep110_Xn165_Xc120/truth1/DAOD_TRUTH1.aod.pool.root"
+#fileName_benchmark_700 = "/cluster/home/amyrewoldt/DAOD/3lep_Hplus700_slep110_Xn165_Xc120/truth1/DAOD_TRUTH1.aod.pool.root"
+#fileName_benchmark_800 = "/cluster/home/amyrewoldt/DAOD/3lep_Hplus800_slep110_Xn165_Xc120/truth1/DAOD_TRUTH1.aod.pool.root"
+#fileName_benchmark_1000 = "/cluster/home/amyrewoldt/DAOD/3lep_benchmark_Hplus1000_slep110/truth1/DAOD_TRUTH1.aod.pool.root"
+#fileName_benchmark_800_fixed = "/cluster/home/amyrewoldt/DAOD/3lep_Hplus800_slep110_Xn165_Xc120_fixed/truth1/DAOD_TRUTH1.test.pool.root"
+fileName_benchmark_500 = "/cluster/home/amyrewoldt/DAOD/3lep_500GeV_100KDataset/truth1/DAOD_TRUTH1.aod.pool.root"
+fileName_benchmark_600 = "/cluster/home/amyrewoldt/DAOD/3lep_600GeV_100KDataset/truth1/DAOD_TRUTH1.aod.pool.root"
+fileName_benchmark_700 = "/cluster/home/amyrewoldt/DAOD/3lep_700GeV_100KDataset/truth1/DAOD_TRUTH1.aod.pool.root"
+fileName_benchmark_800 = "/cluster/home/amyrewoldt/DAOD/3lep_800GeV_100KDataset/truth1/DAOD_TRUTH1.aod.pool.root"
+fileName_benchmark_1000 = "/cluster/home/amyrewoldt/DAOD/3lep_1000GeV_100KDataset/truth1/DAOD_TRUTH1.aod.pool.root"
 
 fileName = [fileName_benchmark_500,fileName_benchmark_600,fileName_benchmark_700,fileName_benchmark_800,fileName_benchmark_1000,fileName_ttb] #,fileName_benchmark_1000_slep_110]
 #fileName = [fileName_ttb] #,fileName_benchmark_1000_slep_110]
@@ -61,23 +81,23 @@ for file in range(len(fileName)):
   nevents = t.GetEntries()
 
   if '500' in fileName[file]:
-    name = 'sig500-allvar-btag.csv'
+    name = 'sig500-allvar-btag-extended-dataset.csv'
   if '600' in fileName[file]:
-    name = 'sig600-allvar-btag.csv'
+    name = 'sig600-allvar-btag-extended-dataset.csv'
   if '700' in fileName[file]:
-    name = 'sig700-allvar-btag.csv'
+    name = 'sig700-allvar-btag-extended-dataset.csv'
   if '800' in fileName[file]:
-    name = 'sig800-allvar-btag.csv'
+    name = 'sig800-allvar-btag-extended-dataset.csv'
   if '1000' in fileName[file]:
-    name = 'sig1000-allvar-btag.csv'
+    name = 'sig1000-allvar-btag-extended-dataset.csv'
   if 'ttb' in fileName[file]:
-    name = 'bkg-allvar-btag.csv'
+    name = 'bkg-allvar-btag-extended-dataset.csv'
 
   ##========================
   print "Working on ", fileName[file]
 
-  DR = []
-  Mll = []
+#  DR = []
+#  Mll = []
   lepton1_pt = []
   lepton2_pt = []
   lepton3_pt = []
@@ -100,7 +120,8 @@ for file in range(len(fileName)):
   dilep_mass = [] 
   transverse_mass = []
 
-  for entry in xrange(9999):
+#  for entry in xrange(1000):
+  for entry in xrange(nevents):
     t.GetEntry(entry)
     taus = t.TruthTaus
     bsm = t.TruthBSM
@@ -145,13 +166,17 @@ for file in range(len(fileName)):
         if m.p4().DeltaR(b.p4()) < .4:
           muon_list.append(m)
     
-    lepton_list = electron_list + muon_list
+    leps = electron_list + muon_list
+#    lepton_list = electron_list + muon_list
 #    print len(lepton_list)
     electron_list.sort(reverse = True, key=(lambda l: l.p4().Pt()))
     muon_list.sort(reverse = True, key=(lambda l: l.p4().Pt()))
-    lepton_list.sort(reverse = True, key=(lambda l: l.p4().Pt()))
-    
-    if len(lepton_list) < 3: continue
+    leps.sort(reverse = True, key=(lambda l: l.p4().Pt()))
+#    lepton_list.sort(reverse = True, key=(lambda l: l.p4().Pt()))
+    if len(leps) < 3: continue
+#    if len(lepton_list) < 3: continue
+#    if abs(lepton_list[0].pdgId() + lepton_list[1].pdgId() + lepton_list[2].pdgId()) not in [11,13]: continue
+    lepton_list.append(selectLeptons(leps)) 
     lepton1_pt.append(lepton_list[0].p4().Pt())
     lepton2_pt.append(lepton_list[1].p4().Pt())
     lepton3_pt.append(lepton_list[2].p4().Pt())
@@ -183,10 +208,9 @@ for file in range(len(fileName)):
       p4 = lepton_list[pair[0]].p4() + lepton_list[pair[1]].p4()
       Mll = p4.M()
       DR = lepton_list[pair[0]].p4().DeltaR(lepton_list[pair[1]].p4())
-      break 
+      break
     dilep_mass.append(Mll)
     dilep_DR.append(DR)
-    print len(dilep_mass), len(dilep_DR)
 
   data = zip([lepton1_pt,lepton2_pt,lepton3_pt,lepton1_eta,lepton2_eta,lepton3_eta,lepton1_phi,lepton2_phi,lepton3_phi,MET,MET_phi,lepton1_flavor, lepton2_flavor, lepton3_flavor,lepton1_charge,lepton2_charge,lepton3_charge,visible_pt,transverse_mass,dilep_mass,dilep_DR])
   with open(name, mode='w') as df:
